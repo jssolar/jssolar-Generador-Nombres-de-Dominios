f2e42bf7ce1b577c497cd66b12324299844e6266
JuanSolar <juansolarsolar@yahoo.com>
2023-05-11 01:27:01 +0000

    generacion de dominios terminado

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var pronoun = ["the", "our"];
+var pronoun = ["the", "our"];{
 var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let adjetivo = 0; adjetivo < adj.length; adjetivo++) {
   
-    for (let sustantivo = 0; sustantivo < noun.length; sustantivo++){
+    for (let sustantivo = 0; sustantivo < noun.length; sustantivo++)
     
-  console.log(pronoun[i]+ "" + adj[adjetivo] + noun[sustantivo]+ ".com");
-  }
+    console.log(pronoun[i]+ + adj[adjetivo] + noun[sustantivo]+ ".com");
+}
 }
 }
